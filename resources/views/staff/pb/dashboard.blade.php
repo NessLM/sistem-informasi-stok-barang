ac80d27c3078d2e24ec336060eba722c2da110ae
Nessalyn <149561544+NessLM@users.noreply.github.com>
2025-09-08 11:55:32 +0700

    Hanya mengubah :title menjadi title, di pj dan pb pada dashboard.blade

--- resources/views/staff/pb/dashboard.blade.php
+++ resources/views/staff/pb/dashboard.blade.php
@@ -1,4 +1,4 @@
-<x-layouts.app :title="'Dashboard • Pembantu Bendahara'" :menu="$menu">
+<x-layouts.app title="'Dashboard • Pembantu Bendahara'" :menu="$menu">
     <header class="page"><h1>Dashboard</h1></header>
     <section class="card">
       <p>Selamat datang, {{ auth()->user()->nama }} — Anda masuk sebagai Pembantu Bendahara.</p>
