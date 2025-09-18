2ea40ba82d51f29ae2eda694146eb44ea29fc7a6
Arya Ramadhani <okearya.projects@gmail.com>
2025-09-18 10:44:14 +0700

    nambah menu helper pb,pj

--- app/Helpers/MenuHelper.php
+++ app/Helpers/MenuHelper.php
@@ -36,4 +36,36 @@ class MenuHelper
             ['label' => 'Data Pengguna', 'icon' => 'bi-people', 'route' => 'admin.users.index'],
         ];
     }
+
+    public static function pbMenu()
+    {
+        return [
+            ['label' => 'Dashboard', 'icon' => 'bi-grid', 'route' => 'staff.admin.dashboard'],
+
+            ['label' => 'Kelola Barang', 'icon' => 'bi-card-list', 'children' => [
+                ['label' => 'Gudang ATK',         'icon' => 'bi-grid', 'route' => 'staff.admin.dashboard'],
+                ['label' => 'Gudang Listrik',     'icon' => 'bi-grid', 'route' => 'staff.admin.dashboard'],
+                ['label' => 'Gudang Kebersihan',  'icon' => 'bi-grid', 'route' => 'staff.admin.dashboard'],
+                ['label' => 'Gudang B Komputer',  'icon' => 'bi-grid', 'route' => 'staff.admin.dashboard'],
+            ]],
+
+            ['label' => 'Riwayat',        'icon' => 'bi-clock-history', 'route' => 'staff.admin.dashboard'],
+            ['label' => 'Laporan',        'icon' => 'bi-file-earmark-bar-graph-fill', 'route' => 'staff.admin.dashboard'],
+        ];
+    }
+
+    public static function pjMenu()
+    {
+        return [
+            ['label' => 'Dashboard', 'icon' => 'bi-grid', 'route' => 'staff.admin.dashboard'],
+
+            ['label' => 'Data Keseluruhan', 'icon' => 'bi-card-list', 'children' => [
+                ['label' => 'Gudang ATK',         'icon' => 'bi-grid', 'route' => 'staff.admin.dashboard'],
+                ['label' => 'Gudang B Komputer',  'icon' => 'bi-grid', 'route' => 'staff.admin.dashboard'],
+            ]],
+            ['label' => 'Riwayat',        'icon' => 'bi-clock-history', 'route' => 'staff.admin.dashboard'],
+        ];
+    }
 }
+
+
