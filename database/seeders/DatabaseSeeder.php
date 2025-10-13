d60edbde4bb377e25892d4f9aa4179767700d3e6
Arya Ramadhani <okearya.projects@gmail.com>
2025-10-13 11:02:45 +0700

    Perbaikan GudangSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RoleSeeder::class,
             GudangSeeder::class,
-             StokGudangSeeder::class,
+            StokGudangSeeder::class,
             UserSeeder::class
         ]);
     }
